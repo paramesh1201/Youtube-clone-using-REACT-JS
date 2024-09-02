b273bd45cfedcd5775eafabea6efe943de4e1e03
paramesh1201 <720721104129@hicet.ac.in>
2024-09-02 20:24:15 +0530

    youtube

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom/client'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import Navbar from './navbar'
+import Icons from './icons'
+import Video from './videos'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const Body = () => {
+    return (
+      <div className="body">
+        <div className="first">
+          <Navbar />
+        </div>
+        <div className="second">
+          <Icons />
+          <Video/>
+        </div>
+      </div>
+    )
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Body />);
